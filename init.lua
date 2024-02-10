40486982295c55bb8498a6104948bb0133dde904
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-02-09 19:27:02 -0700

    v1.0_test:
    * Made the window not scrollable.
    * Made the target buffs section a child region that is scrollable.
    * Tweaked some of the playerinfo sizing and scaling.
    * Re-ordered the level and class text, would rather the class chop when the window is to small than the level.
    * Added in logic check for zoning. would sometimes crash, typically when camping to char select.

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ local anim = mq.FindTextureAnimation('A_SpellIcons')
 local TLO = mq.TLO
 local ME = TLO.Me
 local TARGET = TLO.Target
-local winFlag = bit32.bor(ImGuiWindowFlags.NoTitleBar)
+local winFlag = bit32.bor(ImGuiWindowFlags.NoTitleBar, ImGuiWindowFlags.NoScrollbar, ImGuiWindowFlags.NoScrollWithMouse)
 local pulse = true
 local textureWidth = 20
 local textureHeight = 20
@@ -76,10 +76,6 @@ function DrawStatusIcon(iconID, txt)
     local cursor_x, cursor_y = ImGui.GetCursorPos()
     anim:SetTextureCell(iconID or 0)
     ImGui.DrawTextureAnimation(anim, textureWidth-5, textureHeight-5)
-    --ImGui.SetCursorPos(cursor_x, cursor_y)
-    --ImGui.PushID(tostring(iconID) .. spell.Name() .. "_invis_btn")
-    --ImGui.InvisibleButton(spell.Name(), ImVec2(textureWidth, textureHeight),
-    -- bit32.bor(ImGuiButtonFlags.MouseButtonRight))
     if ImGui.IsItemHovered() then
         ImGui.BeginTooltip()
         ImGui.Text(txt)
@@ -94,7 +90,7 @@ local function targetBuffs(count)
     local iconsDrawn = 0
     -- Calculate max icons per row based on the window width
     local windowWidth = ImGui.GetWindowContentRegionWidth()
-    local maxIconsRow = (windowWidth) / (textureWidth-0.5)
+    local maxIconsRow = (windowWidth) / (textureWidth)
     ImGui.BeginGroup()
     for i = 1, count do
         local sIcon = TARGET.Buff(i).SpellIcon()
@@ -120,203 +116,219 @@ local function getConLevel(spawn)
     return conColor
 end
 function GUI_Target(open)
+    --Rounded corners
     ImGui.PushStyleVar(ImGuiStyleVar.FrameRounding, 5)
     -- Default window size
     ImGui.SetNextWindowSize(216,239, ImGuiCond.FirstUseEver)
     local show = false
     open, show = ImGui.Begin("Target", open, winFlag)
     if not show then
+        ImGui.PopStyleVar()
         ImGui.End()
         return open
     end
-    -- Combat Status
-    if ME.Combat() then
-        ImGui.SetItemAllowOverlap()
-        --ImGui.SetCursorPosY(10)
-        ImGui.SetCursorPosX((ImGui.GetContentRegionAvail()/2)-22)
-        if pulse then
-            COLOR.barColor('pink')
-            pulse = false
-            else
-            COLOR.barColor('red')
-            pulse = true
-        end
-        ImGui.ProgressBar(1, 21, 20, '##c')
-        --ImGui.Text(Icons.MD_LENS)
-        ImGui.PopStyleColor()
-    end
-    ImGui.SameLine()
-    ImGui.SetCursorPosX(5)
-    --ImGui.SetCursorPosY(10)
-    -- Player Information
-    ImGui.BeginGroup()
-    if ImGui.BeginTable("##playerInfo", 4, tPlayerFlags) then
-        ImGui.TableSetupColumn("##tName", ImGuiTableColumnFlags.NoResize,(ImGui.GetContentRegionAvail()*.5))
-        ImGui.TableSetupColumn("##tVis", ImGuiTableColumnFlags.NoResize,16)
-        ImGui.TableSetupColumn("##tIcons", ImGuiTableColumnFlags.WidthStretch,ImGui.GetContentRegionAvail()*.2)
-        ImGui.TableSetupColumn("##tClass", ImGuiTableColumnFlags.NoResize, 60)
-        ImGui.TableNextRow()
-        -- Name
-        ImGui.SetWindowFontScale(1)
-        ImGui.TableSetColumnIndex(0)
-        local meName = ME.CleanName()
-        ImGui.Text(meName)
-        local combatState = mq.TLO.Me.CombatState()
-        if combatState=='COMBAT' then
-            ImGui.SameLine(ImGui.GetColumnWidth()-25)
-            DrawStatusIcon(50,'Combat')
-        end
-        -- Visiblity
-        ImGui.TableSetColumnIndex(1)
-        if TARGET() ~= nil then
-            if TARGET.LineOfSight() then
-                ImGui.PushStyleColor(ImGuiCol.Text, 0, 1, 0, 1)
-                ImGui.Text(Icons.MD_VISIBILITY)
-                ImGui.PopStyleColor()
+    if not TLO.Me.Zoning() then
+        -- Combat Status
+        if ME.Combat() then
+            ImGui.SetItemAllowOverlap()
+            --ImGui.SetCursorPosY(10)
+            ImGui.SetCursorPosX((ImGui.GetContentRegionAvail()/2)-22)
+            if pulse then
+                COLOR.barColor('pink')
+                pulse = false
                 else
-                ImGui.PushStyleColor(ImGuiCol.Text, 0.9, 0, 0, 1)
-                ImGui.Text(Icons.MD_VISIBILITY_OFF)
-                ImGui.PopStyleColor()
+                COLOR.barColor('red')
+                pulse = true
             end
-        end
-        ImGui.SetWindowFontScale(.91)
-        -- Icons
-        ImGui.TableSetColumnIndex(2)
-        ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, 0, 0)
-        ImGui.Text('')
-        if TLO.Group.MainTank.ID() == ME.ID() then
-            ImGui.SameLine()
-            DrawStatusIcon(46,'Main Tank')
-        end
-        if TLO.Group.MainAssist.ID() == ME.ID() then
-            ImGui.SameLine()
-            DrawStatusIcon(49,'Main Assist')
+            ImGui.ProgressBar(1, 21, 20, '##c')
+            --ImGui.Text(Icons.MD_LENS)
+            ImGui.PopStyleColor()
         end
         ImGui.SameLine()
-        --  ImGui.SameLine()
-        ImGui.Text('')
-        ImGui.PopStyleVar()
-        -- Class & Lvl
-        ImGui.SetWindowFontScale(.75)
-        ImGui.TableSetColumnIndex(3)
-        ImGui.Text(ME.Class.ShortName())
-        ImGui.SameLine()
-        ImGui.SetWindowFontScale(.91)
-        ImGui.Text(tostring(ME.Level()))
-        ImGui.EndTable()
-    end
-    ImGui.Separator()
-    -- My Health Bar
-    ImGui.SetWindowFontScale(0.75)
-    COLOR.barColor('red')
-    ImGui.ProgressBar(((tonumber(ME.PctHPs() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##'..ME.PctHPs())
-    ImGui.PopStyleColor()
-    ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
-    ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ((ImGui.GetWindowWidth()/2)-8))
-    ImGui.Text(tostring(ME.PctHPs()))
-    --My Mana Bar
-    if (tonumber(ME.MaxMana())>0) then
-        COLOR.barColor('blue')
-        ImGui.ProgressBar(((tonumber(ME.PctMana() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##' ..ME.PctMana())
+        ImGui.SetCursorPosX(5)
+        --ImGui.SetCursorPosY(10)
+        -- Player Information
+        ImGui.BeginGroup()
+        if ImGui.BeginTable("##playerInfo", 4, tPlayerFlags) then
+            ImGui.TableSetupColumn("##tName", ImGuiTableColumnFlags.NoResize,(ImGui.GetContentRegionAvail()*.5))
+            ImGui.TableSetupColumn("##tVis", ImGuiTableColumnFlags.NoResize,16)
+            ImGui.TableSetupColumn("##tIcons", ImGuiTableColumnFlags.WidthStretch,60)   --ImGui.GetContentRegionAvail()*.25)
+            ImGui.TableSetupColumn("##tClass", ImGuiTableColumnFlags.NoResize,55)
+            ImGui.TableNextRow()
+            -- Name
+            ImGui.SetWindowFontScale(1)
+            ImGui.TableSetColumnIndex(0)
+            local meName = ME.CleanName()
+            ImGui.Text(meName)
+            local combatState = mq.TLO.Me.CombatState()
+            if combatState=='COMBAT' then
+                ImGui.SameLine(ImGui.GetColumnWidth()-25)
+                DrawStatusIcon(50,'Combat')
+            end
+            -- Visiblity
+            ImGui.TableSetColumnIndex(1)
+            if TARGET() ~= nil then
+                if TARGET.LineOfSight() then
+                    ImGui.PushStyleColor(ImGuiCol.Text, 0, 1, 0, 1)
+                    ImGui.Text(Icons.MD_VISIBILITY)
+                    ImGui.PopStyleColor()
+                    else
+                    ImGui.PushStyleColor(ImGuiCol.Text, 0.9, 0, 0, 1)
+                    ImGui.Text(Icons.MD_VISIBILITY_OFF)
+                    ImGui.PopStyleColor()
+                end
+            end
+            ImGui.SetWindowFontScale(.91)
+            -- Icons
+            ImGui.TableSetColumnIndex(2)
+            ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, 0, 0)
+            ImGui.Text('')
+            if TLO.Group.MainTank.ID() == ME.ID() then
+                ImGui.SameLine()
+                DrawStatusIcon(46,'Main Tank')
+            end
+            if TLO.Group.MainAssist.ID() == ME.ID() then
+                ImGui.SameLine()
+                DrawStatusIcon(49,'Main Assist')
+            end
+            ImGui.SameLine()
+            --  ImGui.SameLine()
+            ImGui.Text('')
+            ImGui.PopStyleVar()
+            -- Class & Lvl
+            ImGui.TableSetColumnIndex(3)
+            ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, 2, 0)
+            ImGui.SetWindowFontScale(.91)
+            ImGui.Text(tostring(ME.Level()))
+            --ImGui.Text(tostring('125'))
+            ImGui.SameLine()
+            ImGui.SetWindowFontScale(.75)
+            ImGui.Text(ME.Class.ShortName())
+            --ImGui.Text('UNK')
+            ImGui.PopStyleVar()
+            ImGui.EndTable()
+        end
+        ImGui.Separator()
+        -- My Health Bar
+        ImGui.SetWindowFontScale(0.75)
+        COLOR.barColor('red')
+        ImGui.ProgressBar(((tonumber(ME.PctHPs() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##'..ME.PctHPs())
         ImGui.PopStyleColor()
         ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
         ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ((ImGui.GetWindowWidth()/2)-8))
-        ImGui.Text(tostring(ME.PctMana()))
-    end
-    --My Endurance barB
-    COLOR.barColor('yellow')
-    ImGui.ProgressBar(((tonumber(ME.PctEndurance() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##'..ME.PctEndurance())
-    ImGui.PopStyleColor()
-    ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
-    ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ((ImGui.GetWindowWidth()/2)-8))
-    ImGui.Text(tostring(ME.PctEndurance()))
-    ImGui.Separator()
-    ImGui.EndGroup()
-    if ImGui.IsItemHovered() and ImGui.IsMouseReleased(ImGuiMouseButton.Left) then
-        mq.cmd('/target  ${Me}')
-    end
-    --Target Info
-    if (TARGET()~= nil) then
-        --Target Health Bar
-        COLOR.barColor('red')
-        ImGui.ProgressBar(((tonumber(TARGET.PctHPs()or 0))/100), ImGui.GetContentRegionAvail(), 30, '##'..TARGET.PctHPs())
-        ImGui.PopStyleColor()
-        ImGui.SetWindowFontScale(0.9)
-        ImGui.SetCursorPosY(ImGui.GetCursorPosY()-37)
-        ImGui.SetCursorPosX(9)
-        if ImGui.BeginTable("##targetInfoOverlay", 2, tPlayerFlags) then
-            ImGui.TableSetupColumn("##col1", ImGuiTableColumnFlags.NoResize,(ImGui.GetContentRegionAvail()*.5) - 8)
-            ImGui.TableSetupColumn("##col2", ImGuiTableColumnFlags.NoResize,(ImGui.GetContentRegionAvail()*.5) +8) -- Adjust width for distance and Aggro% text
-            -- First Row: Name, con, distance
-            ImGui.TableNextRow()
-            ImGui.TableSetColumnIndex(0) -- Name and CON in the first column
-            ImGui.SetWindowFontScale(0.9)
-            local targetName = TARGET.CleanName()
-            ImGui.Text(targetName)
-            -- Distance in the second column
-            ImGui.TableSetColumnIndex(1)
-            local tC = getConLevel(TARGET)
-            COLOR.txtColor(tC)
-            if tC == 'red' then
-                ImGui.Text('   '..Icons.MD_WARNING)
-                else
-                ImGui.Text('   '..Icons.MD_LENS)
-            end
+        ImGui.Text(tostring(ME.PctHPs()))
+        --My Mana Bar
+        if (tonumber(ME.MaxMana())>0) then
+            COLOR.barColor('blue')
+            ImGui.ProgressBar(((tonumber(ME.PctMana() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##' ..ME.PctMana())
             ImGui.PopStyleColor()
-            ImGui.SameLine(ImGui.GetColumnWidth()-35)
-            COLOR.txtColor('yellow')
-            ImGui.Text(tostring(math.floor(TARGET.Distance() or 0))..'m')
-            ImGui.PopStyleColor()
-            -- Second Row: Class, Level, and Aggro%
-            ImGui.TableNextRow()
-            ImGui.TableSetColumnIndex(0) -- Class and Level in the first column
-            local tClass = TARGET.Class.ShortName() == 'UNKNOWN CLASS' and Icons.MD_HELP_OUTLINE or TARGET.Class.ShortName()
-            local tLvl = TARGET.Level() or 0
-            ImGui.Text(tostring(tLvl)..' '..tClass)
-            -- Aggro% text in the second column
-            ImGui.TableSetColumnIndex(1)
-            ImGui.SetWindowFontScale(1)
-            ImGui.Text(tostring(TARGET.PctHPs())..'%')
-            ImGui.EndTable()
-        end
-        ImGui.SetWindowFontScale(0.75)
-        ImGui.Separator()
-        --Aggro % Bar
-        if (TARGET.Aggressive) then
-            COLOR.barColor('purple')
-            ImGui.ProgressBar(((tonumber(TARGET.PctAggro() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##'..TARGET.PctAggro())
-            ImGui.PopStyleColor()
-            --Secondary Aggro Person
-            if (TARGET.SecondaryAggroPlayer()~= nil) then
-                ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
-                ImGui.SetCursorPosX(ImGui.GetCursorPosX() + 5)
-                ImGui.Text(TARGET.SecondaryAggroPlayer.CleanName())
-            end
-            --Aggro % Label middle of bar
             ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
             ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ((ImGui.GetWindowWidth()/2)-8))
-            ImGui.Text(TARGET.PctAggro())
-            if (TARGET.SecondaryAggroPlayer()~= nil) then
-                ImGui.SetCursorPosY(ImGui.GetCursorPosY()-18)
-                ImGui.SetCursorPosX(ImGui.GetWindowWidth()-40)
-                ImGui.Text(TARGET.SecondaryPctAggro())
+            ImGui.Text(tostring(ME.PctMana()))
+        end
+        --My Endurance barB
+        COLOR.barColor('yellow')
+        ImGui.ProgressBar(((tonumber(ME.PctEndurance() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##'..ME.PctEndurance())
+        ImGui.PopStyleColor()
+        ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
+        ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ((ImGui.GetWindowWidth()/2)-8))
+        ImGui.Text(tostring(ME.PctEndurance()))
+        ImGui.Separator()
+        ImGui.EndGroup()
+        if ImGui.IsItemHovered() and ImGui.IsMouseReleased(ImGuiMouseButton.Left) then
+            mq.cmd('/target  ${Me}')
+        end
+        --Target Info
+        if (TARGET()~= nil) then
+            --Target Health Bar
+            COLOR.barColor('red')
+            ImGui.ProgressBar(((tonumber(TARGET.PctHPs()or 0))/100), ImGui.GetContentRegionAvail(), 30, '##'..TARGET.PctHPs())
+            ImGui.PopStyleColor()
+            ImGui.SetWindowFontScale(0.9)
+            ImGui.SetCursorPosY(ImGui.GetCursorPosY()-37)
+            ImGui.SetCursorPosX(9)
+            if ImGui.BeginTable("##targetInfoOverlay", 2, tPlayerFlags) then
+                ImGui.TableSetupColumn("##col1", ImGuiTableColumnFlags.NoResize,(ImGui.GetContentRegionAvail()*.5) - 8)
+                ImGui.TableSetupColumn("##col2", ImGuiTableColumnFlags.NoResize,(ImGui.GetContentRegionAvail()*.5) +8) -- Adjust width for distance and Aggro% text
+                -- First Row: Name, con, distance
+                ImGui.TableNextRow()
+                ImGui.TableSetColumnIndex(0) -- Name and CON in the first column
+                ImGui.SetWindowFontScale(0.9)
+                local targetName = TARGET.CleanName()
+                ImGui.Text(targetName)
+                -- Distance in the second column
+                ImGui.TableSetColumnIndex(1)
+                local tC = getConLevel(TARGET)
+                COLOR.txtColor(tC)
+                if tC == 'red' then
+                    ImGui.Text('   '..Icons.MD_WARNING)
+                    else
+                    ImGui.Text('   '..Icons.MD_LENS)
+                end
+                ImGui.PopStyleColor()
+                ImGui.SameLine(ImGui.GetColumnWidth()-35)
+                COLOR.txtColor('yellow')
+                ImGui.Text(tostring(math.floor(TARGET.Distance() or 0))..'m')
+                ImGui.PopStyleColor()
+                -- Second Row: Class, Level, and Aggro%
+                ImGui.TableNextRow()
+                ImGui.TableSetColumnIndex(0) -- Class and Level in the first column
+                local tClass = TARGET.Class.ShortName() == 'UNKNOWN CLASS' and Icons.MD_HELP_OUTLINE or TARGET.Class.ShortName()
+                local tLvl = TARGET.Level() or 0
+                ImGui.Text(tostring(tLvl)..' '..tClass)
+                -- Aggro% text in the second column
+                ImGui.TableSetColumnIndex(1)
+                ImGui.SetWindowFontScale(1)
+                ImGui.Text(tostring(TARGET.PctHPs())..'%')
+                ImGui.EndTable()
+            end
+            ImGui.SetWindowFontScale(0.75)
+            ImGui.Separator()
+            --Aggro % Bar
+            if (TARGET.Aggressive) then
+                COLOR.barColor('purple')
+                ImGui.ProgressBar(((tonumber(TARGET.PctAggro() or 0))/100), ImGui.GetContentRegionAvail(), 10, '##'..TARGET.PctAggro())
+                ImGui.PopStyleColor()
+                --Secondary Aggro Person
+                if (TARGET.SecondaryAggroPlayer()~= nil) then
+                    ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
+                    ImGui.SetCursorPosX(ImGui.GetCursorPosX() + 5)
+                    ImGui.Text(TARGET.SecondaryAggroPlayer.CleanName())
+                end
+                --Aggro % Label middle of bar
+                ImGui.SetCursorPosY(ImGui.GetCursorPosY()-15)
+                ImGui.SetCursorPosX(ImGui.GetCursorPosX() + ((ImGui.GetWindowWidth()/2)-8))
+                ImGui.Text(TARGET.PctAggro())
+                if (TARGET.SecondaryAggroPlayer()~= nil) then
+                    ImGui.SetCursorPosY(ImGui.GetCursorPosY()-18)
+                    ImGui.SetCursorPosX(ImGui.GetWindowWidth()-40)
+                    ImGui.Text(TARGET.SecondaryPctAggro())
+                end
+                else
+                ImGui.Text('')
+            end
+            ImGui.Separator()
+            --Target Buffs
+            if tonumber(TARGET.BuffCount()) > 0 then
+                local windowWidth, windowHeight = ImGui.GetContentRegionAvail()
+                -- Begin a scrollable child
+                ImGui.BeginChild("TargetBuffsScrollRegion", ImVec2(windowWidth, windowHeight), true)
+                targetBuffs(tonumber(TARGET.BuffCount()))
+                ImGui.EndChild()
+                -- End the scrollable region
+                ImGui.Separator()
             end
             else
             ImGui.Text('')
         end
-        ImGui.Separator()
-        --Target Buffs
-        if tonumber(TARGET.BuffCount()) > 0 then
-            ImGui.SetCursorPosX(1)
-            targetBuffs(tonumber(TARGET.BuffCount()))
-            ImGui.Separator()
-        end
+        ImGui.PopStyleVar()
+        ImGui.Spacing()
+        ImGui.End()
         else
-        ImGui.Text('')
+        ImGui.PopStyleVar()
+        ImGui.Spacing()
+        ImGui.End()
     end
-    ImGui.PopStyleVar()
-    ImGui.Spacing()
-    ImGui.End()
     return open
 end
 local openGUI = true
